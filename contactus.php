9774d610b4000860cdf2bc7598fa7ed3d574a792
Arash Abtahi <arash281pro@live.com>
2017-10-22 13:43:23 -0500

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Salon in Katy - Paris Gallery Hair Salon & Day Spa</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+     <link rel="stylesheet"  href="css/font-awesome-4.7.0/css/font-awesome.min.css">
+     <link rel="stylesheet"  href="css/pari.css">
+     <link href="https://fonts.googleapis.com/css?family=Anton|Maven+Pro" rel="stylesheet">
+      <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.4.1/css/mdb.min.css">
+
+
+
+</head>
+<body>
+
+<header>
+    <div class="navbar navbar-default navbar-fixed-top" style="background-color: #814708;">
+            <div class="container">
+                <div class="navbar_header">
+
+                      <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                       <div class="collapse navbar-collapse">
+
+                      <ul class="nav navbar-nav">
+                          <li><a href="index.html">Home</a></li>
+                          <li><a href="salon.html">Salon Services</a></li>
+                          <li><a href="spa.html">Spa Service</a></li>
+                          <li><a href="facial.html">Facial Service</a></li>
+                          <li><a href="packages.html">Packages</a></li>
+                          <li><a href="index.html#about">About Us</a></li>
+                          <li><a href="product.html">Products</a></li>
+                          <li><a href="index.html#review">Testimonials</a></li>
+                          <li><a href="contactus.php">Contact Us</a></li>
+
+                      </ul>
+                    <ul id="social"  class="nav navbar-nav navbar-right">
+                        <li><a href="https://www.facebook.com/ParisGallerySalonSpa"><i class="fa fa-facebook-official fa-2x facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.instagram.com/parisgallerysalonspa/"><i class="fa fa-instagram fa-2x insta" aria-hidden="true"></i></a></li>
+                        <li id="phone"><i class="fa fa-phone fa-2x" aria-hidden="true"></i></li>
+                        <li class="phone"><a href="tel:2813473177">281-347-3177</a></li>
+
+
+                    </ul>
+                </div>
+
+                </div>
+            </div>
+            </div>
+            </div>
+</header>
+
+<div class="container container-alignment" >
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <div class="panel panel-default">
+            <div class="panel-heading">Contact Us Form</div>
+            <div class="panel-body">
+              <div id="response"></div>
+              <form class="form-horizontal"  id="perfect-contact-form" autocomplete="off">
+                <div class="form-group">
+                <div class="col-sm-6">
+                  <input type="text" name="name" class="form-control bottom-space"  placeholder="Name">
+                </div>
+                <div class="col-sm-6">
+                  <input type="email" name="email" class="form-control" placeholder="Email">
+                </div>
+                </div><!-- end div .form-group-->
+                <div class="form-group">
+                <div class="col-sm-6">
+                  <input type="tel" name="phone" class="form-control bottom-space" placeholder="Phone">
+                </div>
+                <div class="col-sm-6">
+                  <select class="form-control" name="subject" >
+                    <option value="Sales">Spa Service</option>
+                    <option value="Feature">Salon Service</option>
+                    <option value="Bug">Facial</option>
+                    <option>Any Other Questions</option>
+                  </select>
+                </div>
+                </div><!-- end div .form-group-->
+                <div class="form-group">
+                <div class="col-sm-12">
+                  <textarea  class="form-control" name="message" id="quick-acf-message"  placeholder="Message" rows="6"></textarea>
+                </div>
+                </div><!-- end div .form-group-->
+                <div class="form-group">
+                <div class="col-sm-6 text-alignment captcha"></div>
+                <div class="col-sm-6">
+                  <input type="tel" name="captcha" class="form-control" placeholder="Answer it">
+                </div>
+                </div><!-- end div .form-group-->
+                <div class="form-group">
+                <div class="col-sm-2 col-lg-2 col-xs-12">
+                  <input type="hidden" name="mail_type" value="simple_mail" />
+                  <button type="submit" class="btn btn-block btn-success">Submit</button>
+                </div>
+                </div><!-- end div .form-group-->
+              </form><!-- end form-->
+            </div><!-- end div .panel-body-->
+          </div><!-- end div .panel-->
+        </div><!-- end div .col-md-8-->
+      </div><!-- end div .row-->
+    </div>
+
+
+
+
+
+<footer class="page-footer center-on-small-only stylish-color-dark">
+
+    <!--Footer Links-->
+    <div class="container">
+        <div class="row">
+
+            <!--First column-->
+            <div class="col-md-4">
+                <h5 class="title mb-4 mt-3 font-bold">Paris Gallery</h5>
+                <p>Make your appointment today by calling 281-347-3177</p>
+            </div>
+            <!--/.First column-->
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!--Second column-->
+            <div class="col-md-2 mx-auto">
+
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="salon.html">Salon Service</a></li>
+                    <li><a href="spa.html">Spa Service</a></li>
+                    <li><a href="product.html">Products</a></li>
+                </ul>
+            </div>
+            <!--/.Second column-->
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!--Third column-->
+            <div class="col-md-2 mx-auto">
+
+                <ul>
+                    <li><a href="packages.html">Packages</a></li>
+                    <li><a href="index.htmlreview.html">Testimonial</a></li>
+                    <li><a href="contactus.php"></a>Contact Us</li>
+                    <li><a href="career.html">Careers</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+    <!--/.Footer Links-->
+
+    <!--Copyright-->
+    <div class="footer-copyright">
+        <div class="container-fluid">
+
+
+        </div>
+    </div>
+    <!--/.Copyright-->
+
+</footer>
+
+
+
+<script src="js/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.4.1/js/mdb.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/pari.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8UFhYT7QaSzPj0Np74XQB2pddrHV2o2g&callback=initMap"
+    async defer></script>
+
+</body>
+</html>
